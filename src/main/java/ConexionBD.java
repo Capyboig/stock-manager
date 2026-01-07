d1d37d579ef47a5333cea468de320a643aa4af3c
Fran-Gil <Fran@gmail.com>
2026-01-07 21:21:48 +0100

    [07/01/26 20:21] Implementacion Hikari & Mejor rendimiento

--- src/main/java/ConexionBD.java
+++ src/main/java/ConexionBD.java
@@ -1,33 +1,47 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+
 public class ConexionBD {
+    private static HikariDataSource dataSource;
 
-    public static Connection conectar() {
-        Properties props = new Properties();
-        Connection con = null;
-
+    static {
         try {
+        Properties props = new Properties();
+        props.load(new FileInputStream("db.properties"));
 
-            props.load(new FileInputStream("db.properties"));
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(props.getProperty("db.url"));
+        config.setUsername(props.getProperty("db.user"));
+        config.setPassword(props.getProperty("db.password"));
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setIdleTimeout(30000);
+        config.setConnectionTimeout(2000);
+            dataSource = new HikariDataSource(config);
+            System.out.println("[EXITO] Pool inicializada correctamente");
 
 
-            String url = props.getProperty("db.url");
-            String user = props.getProperty("db.user");
-            String pass = props.getProperty("db.password");
 
-            con = DriverManager.getConnection(url, user, pass);
+
+
 
         } catch (IOException e) {
-            System.out.println(" [ERROR] No se pudo leer el archivo de configuración 'db.properties'");
-        } catch (SQLException e) {
-            System.out.println("[ERROR] de conexión SQL");
-            e.printStackTrace();
+            System.out.println("[ERROR GRAVE] No se encontro el archivo con las claves para entrar ");
+            throw new RuntimeException("[ERROR] No se pudo cargar la configuracion, e");
         }
-        return con;
+    }
+
+
+    public static Connection conectar() throws SQLException {
+        return dataSource.getConnection();
     }
 }
